ef9559950edfbdb8ea502af7d3f9c31ad470e971
apple <piettia320@gmail.com>
2022-05-05 14:43:44 +0200

    Local commit

--- src/main/java/com/example/demo1/Database.java
+++ src/main/java/com/example/demo1/Database.java
@@ -7,18 +7,13 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- * Database
- * @author Julian Jupiter
- *
- */
 public class Database {
 
     private static final Logger logger = Logger.getLogger(Database.class.getName());
     private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String DB_CONNECTION = "jdbc:mysql://192.168.100.109:3306/javafxsample";
-    private static final String DB_USER = "springuser";
-    private static final String DB_PASSWORD = "qwerty";
+    private static final String DB_CONNECTION = "jdbc:mysql://h25.seohost.pl:3306/srv42082_java_1";
+    private static final String DB_USER = "srv42082_java_1";
+    private static final String DB_PASSWORD = "qwerty123$";
 
     private Database() {
 
@@ -42,7 +37,7 @@ public class Database {
             logger.log(Level.SEVERE, exception.getMessage());
         }
         try {
-            connection = DriverManager.getConnection("jdbc:mysql://192.168.100.109:3306/javafxsample?user=springuser&password=qwerty");
+            connection = DriverManager.getConnection(DB_CONNECTION+"?user="+DB_USER+ "&password=" + DB_PASSWORD);
             return connection;
         } catch  (SQLException exception)  {
             System.out.println("3: ");
